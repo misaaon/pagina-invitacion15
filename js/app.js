90065ac0f0c7c1fc5812d9743c755f0ed01e2c69
Misael Aon <misaelaon10@gmail.com>
2023-05-08 23:10:17 -0300

    agregue funcionalidad de boton de musica

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
 
 const corazonInicio = document.getElementById('container-heart');
 const main = document.querySelector('main');
+const audio = new Audio('../multimedia/a-sky-full-of-stars-traducida-al-espanol.wav');
+const btnMusica =  document.getElementById('btn-musica');
 
 inicioApp();
 
@@ -12,4 +14,26 @@ function inicioApp() {
             corazonInicio.classList.add('hidden');
         }, 5000);
     }
+    reproducirAudio();
+}
+
+btnMusica.addEventListener('click', () => {
+    const icon = document.querySelector('#btn')
+    if(audio.paused) {
+        audio.play();
+        icon.classList.remove('fa-volume-xmark');
+        icon.classList.add('fa-music');
+    } else {
+        audio.pause();
+        icon.classList.add('fa-volume-xmark');
+        icon.classList.remove('fa-music');
+    }
+});
+
+function reproducirAudio() {
+    audio.play();
+}
+
+function detenerAudio() {
+    audio.pause();
 }
