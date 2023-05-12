e71c8dfdb1b37c9bd4635c606ae2243445dc7e4d
Misael Aon <misaelaon10@gmail.com>
2023-05-12 18:13:44 -0300

    agregando nueva seccion y nuevas funcionalidades

--- js/app.js
+++ js/app.js
@@ -5,13 +5,16 @@ const main = document.querySelector('main');
 // const audio = new Audio(['../multimedia/a-sky-full-of-stars-traducida-al-espanol.wav']);
 const audio = document.querySelector('audio');
 const btnMusica =  document.getElementById('btn-musica');
+const btnIngresarMusica = document.getElementById('btn-play');
+const btnSinIngresarMusica = document.getElementById('btn-no-play');
+const ponelePlay = document.getElementById('ponele-play');
 
 inicioApp();
 
 function inicioApp() {
     if(main.classList.contains('hidden')) {
         setTimeout( function() {
-            main.classList.remove('hidden');
+            ponelePlay.classList.remove('hidden');
             corazonInicio.classList.add('hidden');
         }, 5000);
     }
@@ -29,6 +32,17 @@ btnMusica.addEventListener('click', () => {
         icon.classList.remove('fa-music');
     }
 });
+
+btnIngresarMusica.addEventListener('click', () => {
+    reproducirAudio();
+    ponelePlay.classList.add('hidden');
+    main.classList.remove('hidden');
+})
+
+btnSinIngresarMusica.addEventListener('click', () => {
+    ponelePlay.classList.add('hidden');
+    main.classList.remove('hidden');
+})
 
 function reproducirAudio() {
     audio.play();
